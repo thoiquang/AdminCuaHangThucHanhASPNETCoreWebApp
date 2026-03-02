272965ead5b0ebada3236039cb08e678960ad65b
thoiquang <ngothoiquang24042004@gmail.com>
2026-03-02 11:33:42 +0700

    update product

--- 22T1020714.Admin/Controllers/ProductController.cs
+++ 22T1020714.Admin/Controllers/ProductController.cs
@@ -8,5 +8,60 @@ namespace _22T1020714.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Create()
+        {
+            ViewBag.Title = "Bổ sung sản phẩm";
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Title = "Cập nhật thông tin sản phẩm";
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            ViewBag.Title = "Xóa sản phẩm";
+            return View();
+        }
+        public IActionResult ListAttribute(int id)
+        {
+            ViewBag.Title = "Danh sách thuộc tính của sản phẩm";
+            return View();
+        }
+         public IActionResult CreateAttribute(int id)
+        {
+            ViewBag.Title = "Bổ sung thuộc tính cho sản phẩm";
+            return View();
+        }
+         public IActionResult EditAttribute(int id)
+        {
+            ViewBag.Title = "Cập nhật thông tin thuộc tính của sản phẩm";
+            return View();
+        }
+         public IActionResult DeleteAttribute(int id)
+        {
+            ViewBag.Title = "Xóa thuộc tính của sản phẩm";
+            return View();
+        }
+        public IActionResult ListPhoto(int id)
+        {
+            ViewBag.Title = "Danh sách hình ảnh của sản phẩm";
+            return View();
+        }
+         public IActionResult CreatePhoto(int id)
+        {
+            ViewBag.Title = "Bổ sung hình ảnh cho sản phẩm";
+            return View();
+        }
+         public IActionResult EditPhoto(int id)
+        {
+            ViewBag.Title = "Cập nhật thông tin hình ảnh của sản phẩm";
+            return View();
+        }
+         public IActionResult DeletePhoto(int id)
+        {
+            ViewBag.Title = "Xóa hình ảnh của sản phẩm";
+            return View();
+        }
     }
 }
--- 22T1020714.Admin/obj/Debug/net8.0/22T1020714.Admin.AssemblyInfo.cs
+++ 22T1020714.Admin/obj/Debug/net8.0/22T1020714.Admin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("22T1020714.Admin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4407af9d5187d89b7253ae8adae9e0173d3428f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("22T1020714.Admin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("22T1020714.Admin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
